efd68aa4aa0a3158d8b277e85d5650f4dd5bcbcf
michaelbede <72706532+michaelbede@users.noreply.github.com>
2020-12-05 18:17:09 +0000

    Sprites have been sheeted, poject updated to latest version of unity and new input system package installed

---
+++ Dig-Dug/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
